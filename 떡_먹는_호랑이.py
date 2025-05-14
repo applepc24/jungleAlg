b5636a0aabbd20fb06583e6a81abcd33ff9089ec
원준석 <melonpc24@gmail.com>
2025-05-14 10:58:44 +0900

    떡먹는 호랑이

---
+++ 떡_먹는_호랑이.py
@@ -0,0 +1,19 @@
+d, k = map(int, input().split())
+
+fibo = [0] * (d+1)
+fibo[1] = 1
+fibo[2] = 1
+
+for i in range(3, d+1):
+    fibo[i] = fibo[i-1] + fibo[i-2]
+
+A = fibo[d - 2]
+B = fibo[d -1]
+
+for x in range(1, k+1):
+    if(k - A * x) % B == 0:
+        y = (k - A * x) // B
+        if y >= 1:
+            print(x)
+            print(y)
+            break
