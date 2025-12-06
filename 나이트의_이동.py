90733da5d561f1bc13ea1cc9dd575c5bb04367c8
원준석 <applepc24@naver.com>
2025-12-06 22:52:42 +0900

    나이트의 이동

--- 나이트의_이동.py
+++ 나이트의_이동.py
@@ -1,17 +1,36 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-N = int(input())
-R,C = map(int, input().split())
 
-if N == 3:
-    if R ==2 and C == 2:
-        print(1)
-    else:
-        print(4)
-else:
-    total = N*N
-    if (R+C) % 2 == 0:
-        print((total + 1) // 2)
-    else:
-        print(total // 2)
+T = int(input())
+dirs = [(2,1), (-2,1), (2, -1), (-2, -1), (1, 2), (-1, 2), (1, -2), (-1, -2)]
+
+for _ in range(T):
+    N = int(input())
+    sx, sy = map(int, input().split())
+    fx, fy = map(int, input().split())
+    
+    if sx == fx and sy == fy:
+        print(0)
+        continue
+    
+    dist = [[-1] * N for _ in range(N)]
+    q= deque()
+    q.append((sx, sy))
+    dist[sx][sy] = 0
+
+    while q:
+        x, y  = q.popleft()
+        if x == fx and y == fy:
+            print(dist[x][y])
+            break
+        
+        for dx, dy in dirs:
+            nx, ny  = x + dx, y +dy
+            if 0 <= nx < N and 0 <= ny < N and dist[nx][ny] == -1:
+                dist[nx][ny] = dist[x][y] + 1
+                q.append((nx,ny))
+
+
+
